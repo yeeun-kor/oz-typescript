94b99cba91e6c6fc0bd116e4eafa0ece8c23a275
yeeun <jovm268@naver.com>
2025-10-01 14:06:26 +0900

    feat : 기본타입 실습코드 작성 및 컴파일 진행

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,32 @@
 "use strict";
-console.log("hello yeeun");
+// 문자열
+let mystring = "yeeun";
+console.log("String : ", mystring);
+// Number
+let myNum = 25;
+console.log("Number", myNum);
+// boolean
+let isOpen = true;
+let isCompleted = false;
+if (isOpen) {
+    console.log("hello we are open!");
+}
+if (!isCompleted) {
+    console.log("job is not completed");
+}
+//null ( 한가지 이상의 타입: 유니언타입으로 활용을 많이 한다. )
+// 객체가 존재하지 않거나, 값이 없음!
+//user 는 문자열이나 null 값을 갖는다.
+let user = null;
+function login(userName) {
+    user = userName;
+}
+function logout() {
+    user = null;
+}
+login("yeeun"); //login함수에서 string으로 받아왔기 때문에 타입은 string
+logout(); //그러나 logout함수에서 null로 재할당을 받아서 타입은 null
+// any 모든 타입 가능 -> 타입의 안정성 포기함.
+let someValue;
+someValue.toString(); // 다 가능
+someValue = false; // 다 가능
--- src/index.ts
+++ src/index.ts
@@ -16,5 +16,22 @@ if (!isCompleted) {
   console.log("job is not completed");
 }
 
-//null
+//null ( 한가지 이상의 타입: 유니언타입으로 활용을 많이 한다. )
 // 객체가 존재하지 않거나, 값이 없음!
+//user 는 문자열이나 null 값을 갖는다.
+let user: string | null = null;
+function login(userName: string) {
+  user = userName;
+}
+
+function logout() {
+  user = null;
+}
+
+login("yeeun"); //login함수에서 string으로 받아왔기 때문에 타입은 string
+logout(); //그러나 logout함수에서 null로 재할당을 받아서 타입은 null
+
+// any 모든 타입 가능 -> 타입의 안정성 포기함.
+let someValue: any;
+someValue.toString(); // 다 가능
+someValue = false; // 다 가능
