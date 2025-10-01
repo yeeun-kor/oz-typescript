c0992d51b42fd3619645ceefe4652419081ccd43
yeeun <jovm268@naver.com>
2025-10-01 13:50:14 +0900

    chore : 기초세팅

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// 문자열
+let mystring: string = "yeeun";
+console.log("String : ", mystring);
+
+// Number
+let myNum: number = 25;
+console.log("Number", myNum);
+
+// boolean
+let isOpen: boolean = true;
+let isCompleted: boolean = false;
+if (isOpen) {
+  console.log("hello we are open!");
+}
+if (!isCompleted) {
+  console.log("job is not completed");
+}
+
+//null
+// 객체가 존재하지 않거나, 값이 없음!
